12f9a0d752a080f41e7c093d7febd565e2f798ae
Negezor <negezor@gmail.com>
2020-02-21 22:01:13 +1100

    chore(build): migrate to rollup-plugin-typescript2

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,40 @@
-import typescript from 'rollup-plugin-typescript';
+import typescriptPlugin from 'rollup-plugin-typescript2';
+
+import { tmpdir } from 'os';
+import { join as pathJoin } from 'path';
 
 import pkg from './package.json';
 
-export default [
-	{
-		input: 'src/index.ts',
-		plugins: [
-			typescript()
-		],
-		output: [
-			{
-				file: `${pkg.main}.js`,
-				format: 'cjs',
-				exports: 'named'
-			},
-			{
-				file: `${pkg.main}.mjs`,
-				format: 'esm'
+const cacheRoot = pathJoin(tmpdir(), '.rpt2_cache');
+
+const src = pathJoin(__dirname, 'src');
+const lib = pathJoin(__dirname, 'lib');
+
+// eslint-disable-next-line import/no-default-export
+export default {
+	input: pathJoin(src, 'index.ts'),
+	plugins: [
+		typescriptPlugin({
+			cacheRoot,
+
+			useTsconfigDeclarationDir: false,
+
+			tsconfigOverride: {
+				outDir: lib,
+				rootDir: src,
+				include: [src]
 			}
-		]
-	}
-];
+		})
+	],
+	output: [
+		{
+			file: `${pkg.main}.js`,
+			format: 'cjs',
+			exports: 'named'
+		},
+		{
+			file: `${pkg.main}.mjs`,
+			format: 'esm'
+		}
+	]
+};
