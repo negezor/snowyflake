2de9d3a4e16b457a5a9007190eed02b462d9e71d
Negezor <negezor@gmail.com>
2021-12-14 20:09:11 +1100

    BREAKING CHANGE: delete default export

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export * from './interfaces';
 export { Epoch } from './constants';
 
 // eslint-disable-next-line import/no-default-export
-export { Snowyflake, Snowyflake as default } from './snowyflake';
+export { Snowyflake } from './snowyflake';
