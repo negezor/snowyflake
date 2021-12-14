38e1757a83533026d6fb54b9dc9324ca2f70842a
Negezor <negezor@gmail.com>
2021-12-14 20:02:43 +1100

    BREAKING CHANGE: delete Epochs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 export * from './interfaces';
 
-export {
-	Epoch,
-
-	/**
-	 * @deprecated
-	 */
-	Epoch as Epochs
-} from './constants';
+export { Epoch } from './constants';
 
 // eslint-disable-next-line import/no-default-export
 export { Snowyflake, Snowyflake as default } from './snowyflake';
